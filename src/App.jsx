d0ea05f1d211ef81b8a4e832880b035581ef2f81
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-01-09 00:09:36 +0100

    for now

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       logo: Object.assign(new Image(), {
         src: "/src/assets/logos/ARS.png",
       }),
-      coefficient: 1.75-0.5,
+      coefficient: 1.75 - 0.5,
     },
     {
       fullName: "Aston Villa",
@@ -186,39 +186,254 @@ const App = () => {
   ];
 
   let randomNumber = Math.floor(Math.random() * 10000);
-  let goals;
+
+  let homeGoals;
 
   if (randomNumber <= 2146) {
-    goals = 0
+    homeGoals = 0;
   } else if (randomNumber <= 5454) {
-    goals = 1
+    homeGoals = 1;
   } else if (randomNumber <= 7904) {
-    goals = 2
+    homeGoals = 2;
   } else if (randomNumber <= 9318) {
-    goals = 3
+    homeGoals = 3;
   } else if (randomNumber <= 9747) {
-    goals = 4
+    homeGoals = 4;
   } else if (randomNumber <= 9884) {
-    goals = 5
+    homeGoals = 5;
   } else if (randomNumber <= 9954) {
-    goals = 6
+    homeGoals = 6;
   } else if (randomNumber <= 9995) {
-    goals = 7
-  } else if (randomNumber <9999) {
-    goals = 8
-  } else if (randomNumber = 9999) {
-    goals = 9
+    homeGoals = 7;
+  } else if (randomNumber < 9999) {
+    homeGoals = 8;
+  } else if ((randomNumber = 9999)) {
+    homeGoals = 9;
   }
 
-  console.log(randomNumber)
-  console.log(goals)
+  let awayGoals;
 
-  let homeGoals = goals + (0.435/2)
-  let awayGoals = goals - (0.435/2)
+  if (randomNumber <= 2146 * 1.2) {
+    awayGoals = 0;
+  } else if (randomNumber <= 5454 * 1.2) {
+    awayGoals = 1;
+  } else if (randomNumber <= 7904 * 1.2) {
+    awayGoals = 2;
+  } else if (randomNumber <= 9318 * 1.2) {
+    awayGoals = 3;
+  } else if (randomNumber <= 9747 * 1.2) {
+    awayGoals = 4;
+  } else if (randomNumber <= 9884 * 1.2) {
+    awayGoals = 5;
+  } else if (randomNumber <= 9954 * 1.2) {
+    awayGoals = 6;
+  } else if (randomNumber <= 9995 * 1.2) {
+    awayGoals = 7;
+  } else if (randomNumber < 9999 * 1.2) {
+    awayGoals = 8;
+  } else if ((randomNumber = 9999 * 1.2)) {
+    awayGoals = 9;
+  }
 
-  let homeAwayDifference = 0.435;
+  console.log(randomNumber);
+  console.log(awayGoals);
 
-  return <>Hello from the app component, the only component</>;
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  shuffle(teamsObject);
+  console.log(teamsObject);
+
+  return (
+    <>
+      <div className="app">
+        <div className="container">
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[0].fullName}</h2>
+              <img
+                src={teamsObject[0].logo.src}
+                alt={teamsObject[0].threeLetterName}
+              />
+            </div>
+            <div className="score">
+              <h1 className="home-score">3</h1>
+              <h1 className="separator">-</h1>
+              <h1 className="away score">2</h1>
+            </div>
+            <div className="away-team">
+              <img
+                src={teamsObject[1].logo.src}
+                alt={teamsObject[1].threeLetterName}
+              />
+              <h2>{teamsObject[1].fullName}</h2>
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[2].fullName}</h2>
+              <img
+                src={teamsObject[2].logo.src}
+                alt={teamsObject[2].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[3].fullName}</h2>
+              <img
+                src={teamsObject[3].logo.src}
+                alt={teamsObject[3].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[4].fullName}</h2>
+              <img
+                src={teamsObject[4].logo.src}
+                alt={teamsObject[4].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[5].fullName}</h2>
+              <img
+                src={teamsObject[5].logo.src}
+                alt={teamsObject[5].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[6].fullName}</h2>
+              <img
+                src={teamsObject[6].logo.src}
+                alt={teamsObject[6].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[7].fullName}</h2>
+              <img
+                src={teamsObject[7].logo.src}
+                alt={teamsObject[7].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[8].fullName}</h2>
+              <img
+                src={teamsObject[8].logo.src}
+                alt={teamsObject[8].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[9].fullName}</h2>
+              <img
+                src={teamsObject[9].logo.src}
+                alt={teamsObject[9].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[10].fullName}</h2>
+              <img
+                src={teamsObject[10].logo.src}
+                alt={teamsObject[10].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[11].fullName}</h2>
+              <img
+                src={teamsObject[11].logo.src}
+                alt={teamsObject[11].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[12].fullName}</h2>
+              <img
+                src={teamsObject[12].logo.src}
+                alt={teamsObject[12].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[13].fullName}</h2>
+              <img
+                src={teamsObject[13].logo.src}
+                alt={teamsObject[13].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[14].fullName}</h2>
+              <img
+                src={teamsObject[14].logo.src}
+                alt={teamsObject[14].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[15].fullName}</h2>
+              <img
+                src={teamsObject[15].logo.src}
+                alt={teamsObject[15].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[16].fullName}</h2>
+              <img
+                src={teamsObject[16].logo.src}
+                alt={teamsObject[16].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[17].fullName}</h2>
+              <img
+                src={teamsObject[17].logo.src}
+                alt={teamsObject[17].threeLetterName}
+              />
+            </div>
+          </div>
+          <div className="single-game">
+            <div className="home-team">
+              <h2>{teamsObject[18].fullName}</h2>
+              <img
+                src={teamsObject[18].logo.src}
+                alt={teamsObject[18].threeLetterName}
+              />
+            </div>
+            <div className="away-team">
+              <h2>{teamsObject[19].fullName}</h2>
+              <img
+                src={teamsObject[19].logo.src}
+                alt={teamsObject[19].threeLetterName}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
